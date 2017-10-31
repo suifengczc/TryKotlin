8edc29aa114e878f03831b14c53be0eadf93ee4a
suifengczc <ssuifengczc@163.com>
2017-10-31 22:21:09 +0800

    first commit

---
+++ src/main/kotlin/DelegatedProperties04/Observable property.kt
@@ -0,0 +1,17 @@
+package DelegatedProperties04
+
+import kotlin.properties.Delegates
+
+//TODO 这里的observable转换成非lambda写法
+class mUser {
+    var name: String by Delegates.observable("nomalName") { prop, old, new ->
+        println("$old -> $new")
+    }
+}
+
+fun main(args: Array<String>) {
+    val u = mUser()
+    u.name = "first"
+    u.name = "second"
+
+}
